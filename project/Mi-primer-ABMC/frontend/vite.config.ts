39a214e31987511ae85b0fa1f6f2e5c9f2b84761
Sebastian Castedo <sebastian.castedoribera,03@gmail.com>
2025-01-30 17:23:41 -0400

    Mas pruebas

--- project/Mi-primer-ABMC/frontend/vite.config.ts
+++ project/Mi-primer-ABMC/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/aprendiendo-gitFlow/",
   build: {
     outDir: '../../../root'
   }
