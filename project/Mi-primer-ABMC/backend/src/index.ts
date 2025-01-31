71f3130dcb917285f0edca01e7a755c21ff4ea4b
Sebastian Castedo <sebastian.castedoribera,03@gmail.com>
2025-01-31 16:32:44 -0400

    Modificando el backend para aws

--- project/Mi-primer-ABMC/backend/src/index.ts
+++ project/Mi-primer-ABMC/backend/src/index.ts
@@ -6,7 +6,7 @@ import './configurations/database';
 
 async function main() {
     try {
-        await app.listen({ port: 4000 });
+        await app.listen({ port: 4000, host: '0.0.0.0' });
     } catch (err) {
         app.log.error(err);
         process.exit(1);
