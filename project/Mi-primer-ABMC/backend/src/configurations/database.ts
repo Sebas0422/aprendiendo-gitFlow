661861c157b3e60c6e8dbe3bf3f7af98928e1461
Sebastian Castedo <sebastian.castedoribera,03@gmail.com>
2025-02-03 09:31:47 -0400

    Agregando certificacion

--- project/Mi-primer-ABMC/backend/src/configurations/database.ts
+++ project/Mi-primer-ABMC/backend/src/configurations/database.ts
@@ -5,8 +5,10 @@ const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://local
 const connectDB = async () => {
   try {
     await mongoose.connect(URI);
+    console.log('Database connected');
   } catch (error) {
     if (error instanceof Error) {
+      console.log(`Error connecting to the database: ${error.message}`);
       throw new Error(`Error connecting to the database: ${error.message}`);
     }
   }
